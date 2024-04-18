b3c83809d3d3f07ac864314bccbf24def66de454
Konstantinos Balaskas <kompalas@physics.auth.gr>
2024-04-18 16:47:57 +0200

    preparing for mixed workload

--- src/workload.py
+++ src/workload.py
@@ -8,16 +8,16 @@ logger = logging.getLogger(__name__)
 class MultiDNNWorkload:
     def __init__(self, dnn_dict, dataset_dict, print_frequency_dict):
         # TODO: Find a workaround with the network-specific transforms
-        assert len(
-                    [arch for arch, net_wrapper in dnn_dict.items() 
-                     if net_wrapper.model.task == DNNType.SemanticSegmantation]
-                ) <= 1, "No more than 1 segmentation DNN per run is supported, " \
-                        "because each DataLoader contains network-specific transforms"
-        assert len(
-                    [arch for arch, net_wrapper in dnn_dict.items() 
-                     if net_wrapper.model.task == DNNType.ObjectDetection]
-                ) <= 1, "No more than 1 detection DNN per run is supported, " \
-                        "because each DataLoader contains network-specific transforms"
+        # assert len(
+        #             [arch for arch, net_wrapper in dnn_dict.items() 
+        #              if net_wrapper.model.task == DNNType.SemanticSegmantation]
+        #         ) <= 1, "No more than 1 segmentation DNN per run is supported, " \
+        #                 "because each DataLoader contains network-specific transforms"
+        # assert len(
+        #             [arch for arch, net_wrapper in dnn_dict.items() 
+        #              if net_wrapper.model.task == DNNType.ObjectDetection]
+        #         ) <= 1, "No more than 1 detection DNN per run is supported, " \
+        #                 "because each DataLoader contains network-specific transforms"
 
         self.dnns = dnn_dict
         self.datasets = dataset_dict
