8bea2849bb10d98e29d4709c6ffd8c98bb77de50
Konstantinos Balaskas <kompalas@physics.auth.gr>
2024-05-28 18:05:51 +0200

    minor ntfy addition

--- run/baseline.sh
+++ run/baseline.sh
@@ -4,3 +4,7 @@ set -eou pipefail
 python3 main.py \
 	--yaml-cfg run/baseline_cfg.yaml \
 	--workload-cfg run/workloads.yaml
+
+
+final_results="$(grep -A 4 "Evaluation results:" logs/ours___2024.05.28-15.48.41.131/ours___2024.05.28-15.48.41.131.log | tail -n 5)"
+curl -d "baseline completed: $final_results" ntfy.sh/hetero-accel
