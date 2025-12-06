7a53cb27bd9d79eac8e2bc8a020261ef950d3789
Welby Seely <welbyseely@gmail.com>
2025-12-06 17:08:10 -0500

    temp

--- setup/install_eas_lab.sh
+++ setup/install_eas_lab.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 git submodule update --init --recursive
 
+LOCAL_DIRECTORY="$HOME/.local"
+
+if [ -d "$LOCAL_DIRECTORY" ]; then
+  echo "$LOCAL_DIRECTORY does exist."
+fi
+
+
 #curl -LO https://github.com/bazelbuild/bazel/releases/download/7.7.1/bazel-7.7.1-linux-x86_64
 #chmod 777 bazel
 #mv bazel-7.7.1-linux-x86_64 bazel
