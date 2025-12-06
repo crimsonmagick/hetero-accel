0a12804a04655598835b120732bd280e3168c789
Welby Seely <welbyseely@gmail.com>
2025-12-06 16:52:27 -0500

    Began writing sh script for server install

---
+++ setup/install_eas_lab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git submodule update --init --recursive
+
+#curl -LO https://github.com/bazelbuild/bazel/releases/download/7.7.1/bazel-7.7.1-linux-x86_64
+#chmod 777 bazel
+#mv bazel-7.7.1-linux-x86_64 bazel
