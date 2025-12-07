66d9b83b25b2a7f9507cb2e21fd9a0e19c1ecb6c
Welby Seely <welbyseely@gmail.com>
2025-12-07 17:12:07 -0500

    Implemented initial install_eas_lab.sh
    * Initializes repo
    * Creates ~/.share directories
    * Installs bazel in ~/.share/bin

--- setup/install_eas_lab.sh
+++ setup/install_eas_lab.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 git submodule update --init --recursive
 
-LOCAL_DIRECTORY="$HOME/.local"
+create_directory () {
+  pending_dir=$1
+  if [ ! -d "$pending_dir" ]; then
+    echo "$pending_dir does not exist."
+    mkdir "$pending_dir"
+    echo "$pending_dir created."
+  else
+    echo "$pending_dir exists"
+  fi
+}
 
-if [ -d "$LOCAL_DIRECTORY" ]; then
-  echo "$LOCAL_DIRECTORY does exist."
-fi
+local_directory="$HOME/.local"
+create_directory "$local_directory"
+create_directory "$local_directory/bin"
+create_directory "$local_directory/share"
+mkdir -p "$HOME/.local/share/accelergy/estimation_plug_ins/accelergy-cacti-plug-in/"
 
+curl -L -o ~/.local/bin/bazel https://github.com/bazelbuild/bazel/releases/download/7.7.1/bazel-7.7.1-linux-x86_64
+chmod 777 ~/.local/bin/bazel
 
-#curl -LO https://github.com/bazelbuild/bazel/releases/download/7.7.1/bazel-7.7.1-linux-x86_64
-#chmod 777 bazel
-#mv bazel-7.7.1-linux-x86_64 bazel
+#pip3 install pycocotools
